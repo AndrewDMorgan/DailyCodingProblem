c6fb15c58c1306b1f69c72e54e8ab09f91dc5826
Andrew Morgan <andrewdagamer741@gmail.com>
2023-06-19 11:13:45 -0600

    Delete problem35.py

--- problem35.py
+++
@@ -1,9 +0,0 @@
-
-def SortRGB(l: list) -> list:
-    num = [0,0,0]  # the number of each character
-    for char in l:
-        num["RGB".index(char)] += 1  # adding up the number of each character
-    return list("R"*num[0] + "G"*num[1] + "B"*num[2])  # returning the sorted version
-
-# testing it
-print(SortRGB(['G', 'B', 'R', 'R', 'B', 'R', 'G']))
